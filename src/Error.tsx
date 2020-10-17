09984c2864758dc59ed59b4c4aec911ec21462bf
Mihai Doarna <dmihai@gmail.com>
2020-10-17 16:19:44 +0300

    load content before rendering react

---
+++ src/Error.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+function Error() {
+  return <div className="App">Content not available!</div>;
+}
+
+export default Error;
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+import Error from './Error';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+axios
+  .get('content.json')
+  .then(response =>
+    ReactDOM.render(
+      <React.StrictMode>
+        <App content={response.data} />
+      </React.StrictMode>,
+      document.getElementById('root'),
+    ),
+  )
+  .catch(error => ReactDOM.render(<Error />, document.getElementById('root')));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
